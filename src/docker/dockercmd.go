e9b0eef7abd258b2dc5f6da215c52dbabd46cb9d
Guillaume Clement <clement.guillaume@live.ca>
2017-10-31 20:52:17 -0400

    Adding dockercmd error handling and test assertion as per: https://davidnix.io/post/error-handling-in-go/

--- src/docker/dockercmd.go
+++ src/docker/dockercmd.go
@@ -2,9 +2,9 @@ package docker
 
 import (
 	"bytes"
-	"fmt"
-	"log"
 	"os/exec"
+	"regexp"
+	"errors"
 )
 
 func IsDockerInstalled() bool{
@@ -17,30 +17,56 @@ func IsDockerInstalled() bool{
 	}
 }
 
-func DockerPull(imageName string){
+//https://golang.org/pkg/strings/#Index
+func GetDockerVersion() (string, error){
+	//Create version parsing regex
+	var versionRegEx = regexp.MustCompile(`[0-9.]+`)
 	
-	cmd := exec.Command("docker", "pull", imageName)
-	//cmd.Stdin = strings.NewReader("some input")
+	cmd := exec.Command("docker", "version")
 	var out bytes.Buffer
 	cmd.Stdout = &out
 	err := cmd.Run()
 	if err != nil {
-		log.Fatal(err)
+		return "", errors.New("Failed to run [docker version] command" + err.Error())
+	} else{
+		
+		output := out.String()
+		matchResult := versionRegEx.FindStringSubmatch(output)
+		
+		//Make sure we have at least one match
+		if len(matchResult) < 1 {
+			return "", errors.New("Failed to parse docker version, result was: " + output)
+		} else{
+			//Get the first match
+			return matchResult[0], nil
+		}
 	}
-	fmt.Println("docker pull:", out.String())
-	
 }
 
-func DockerTag(imageName string){
+func DockerPull(imageName string) (string, error){
 	
-	cmd := exec.Command("docker", "tag", imageName)
-	//cmd.Stdin = strings.NewReader("some input")
+	cmd := exec.Command("docker", "pull", imageName)
+
 	var out bytes.Buffer
 	cmd.Stdout = &out
 	err := cmd.Run()
 	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("docker tag:", out.String())
+		return "", errors.New("Failed to run [docker pull] command" + err.Error())
+	} else{
+		return out.String(), nil
+	}	
+}
+
+func DockerTag(imageName string) (string, error){
 	
+	cmd := exec.Command("docker", "tag", imageName)
+
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	err := cmd.Run()
+	if err != nil {
+		return "", errors.New("Failed to run [docker tag] command" + err.Error())
+	} else{
+		return out.String(), nil
+	}
 }
--- src/docker/dockercmd_test.go
+++ src/docker/dockercmd_test.go
@@ -4,16 +4,37 @@ import (
     "testing"
     "fmt"
     "setup"
+    "regexp"
 )
 
 func TestIsDockerInstalled(t *testing.T) {
-	//TODO Returns success
 	installed := IsDockerInstalled()
 	fmt.Println("Docker Installed:", installed)
 }
 
+func TestGetDockerVersion(t *testing.T) {
+	//Create version matching regex
+	var versionRegEx = regexp.MustCompile(`[0-9.]*`)
+
+	message, err := GetDockerVersion()
+	
+	if err != nil{
+		t.Error("Error while retrieving docker version", err)
+	} else if !versionRegEx.MatchString(message) {
+		t.Error("Docker Version is invalid")
+	} else{
+		fmt.Println("Docker Version:", message)
+	}
+}
+
 func TestDockerPull(t *testing.T) {
 	//TODO Returns success
-	DockerPull("hyperledger/fabric-peer:x86_64-" + setup.Version)
+	message, err := DockerPull("hyperledger/fabric-peer:x86_64-" + setup.Version)
+	
+	if err != nil{
+		t.Error("Error while running docker pull", err)
+	} else{
+		fmt.Println("Docker Pull:", "\n", message)
+	}
 }
 
